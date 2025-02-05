6a43c5c09c2401e2e73ba75a0cf83857f57d51e5
Russel Makani <B00164413@mytudublin.ie>
2025-02-05 11:50:07 +0000

    exercise

---
+++ src/floatdivide.cpp
@@ -0,0 +1,21 @@
+/*  Russel Makani
+    B00164413
+    Float Divide programm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/*  This programm will divide 2 floating point values
+    Declare floating point variables
+    perform calculation
+    print result
+*/
+
+int main(){
+    float a = 2.0 , b = 5.0;
+    cout << a/b << "\n" ; 
+    return 0 ; 
+}
+
---
+++ src/intdivide.cpp
@@ -0,0 +1,21 @@
+/*  Russel Makani
+    B00164413
+    integer Divide programm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/*  This programm will divide 2 integer values
+    Declare integer variables
+    perform calculation
+    print result
+*/
+
+int main(){
+    int a = 2 , b = 5;
+    cout << a/b << "\n" ; 
+    return 0 ; 
+}
+
