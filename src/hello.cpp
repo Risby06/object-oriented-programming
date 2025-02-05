e9fe6b8c1217fce6ab208c489034d21bf806c24d
Russel Makani <B00164413@mytudublin.ie>
2025-02-05 11:25:56 +0000

    Update hello.cpp

---
+++ src/hello.cpp
@@ -0,0 +1,19 @@
+/*  Russel Makani
+    B00164413
+    This program prints out hello world!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/*  main() is where a c++ program starts
+    output the last 13 characters Hello, world!
+    followed by a new line 
+    return a value indicating success
+*/
+
+int main(){
+    cout << "Hello, World!\n"
+    return 0;
+}
