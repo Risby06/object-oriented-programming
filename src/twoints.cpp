39508b7e46660e2b4b132c1e13ee6fa70704a46a
Russel Makani <B00164413@mytudublin.ie>
2025-02-07 10:16:02 +0000

    Create twoints.cpp

---
+++ src/twoints.cpp
@@ -0,0 +1,17 @@
+/*  Russel Makani
+    B00164413
+    Exercise 3
+    two ints programm
+*/
+
+#include <iostream>
+using namespace std;
+
+/*  This programm will ask the user to enter two integers
+    store the integrs as variables
+    find the smaller and lager number
+    find the difference 
+    find the product
+    find the ratio
+    print them on screeeen
+*/
